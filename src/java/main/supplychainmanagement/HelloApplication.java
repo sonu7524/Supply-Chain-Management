e3eed3f4eb0cd58bfa2044c454e07236dac8b2ed
sonu7524 <sonudaryani135@gmail.com>
2022-12-18 13:25:22 +0530

    Header Bar

--- src/java/main/supplychainmanagement/HelloApplication.java
+++ src/java/main/supplychainmanagement/HelloApplication.java
@@ -3,14 +3,60 @@ package main.supplychainmanagement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    public static final int width = 700, height = 600, headerBar = 50;
+
+    Pane bodyPane = new Pane();
+
+    private GridPane headerBar(){
+        TextField searchText = new TextField();
+        Button searchButton = new Button("Search");
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(searchText, 0, 0);
+        gridPane.add(searchButton, 1, 0);
+
+        return gridPane;
+    }
+
+    private GridPane loginPage(){
+        Label emailLabel = new Label("Email");
+        Label passwordLabel = new Label("Password");
+
+        TextField emailTextField = new TextField();
+        PasswordField passwordField = new PasswordField();
+        Button loginButton = new Button("Login");
+
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(emailLabel,0,0);
+        gridPane.add(passwordField,1,1);
+        gridPane.add(passwordLabel,0,1);
+        gridPane.add(loginButton,0,2);
+        return gridPane;
+    }
+
     private Pane createContent(){
         Pane root = new Pane();
+        root.setPrefSize(width, height+headerBar);
+
+        bodyPane.setMinSize(width,height);
+        bodyPane.setTranslateY(headerBar);
+        bodyPane.getChildren().addAll(loginPage());
+
+        root.getChildren().addAll(headerBar(),bodyPane);
         return root;
     }
     @Override
